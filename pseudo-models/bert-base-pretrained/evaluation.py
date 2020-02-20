d090860dfa3a87115f70431e1b5b668c46aa0bd0
Dmitriy Danevskiy <d.danevskyi@gmail.com>
2020-02-20 21:21:32 +0000

    Add source code for pseudo models

---
+++ pseudo-models/bert-base-pretrained/evaluation.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from scipy.stats import spearmanr
+import numpy as np
+
+from misc import target_columns
+
+
+def target_metric(prediction, actual, columns=target_columns):
+
+    prediction = prediction.sort_values(by="qa_id").reset_index(drop=True)
+    actual = actual.sort_values(by="qa_id").reset_index(drop=True)
+
+    assert (prediction.qa_id == actual.qa_id).all()
+
+    score = 0
+    for col in columns:
+        score += np.nan_to_num(
+            spearmanr(prediction[col], actual[col]).correlation
+        ) / len(target_columns)
+    return score
