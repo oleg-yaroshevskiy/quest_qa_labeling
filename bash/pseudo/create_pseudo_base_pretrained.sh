5fdd97f98ac8347f624248561c520fc97d888f44
Dmitriy Danevskiy <d.danevskyi@gmail.com>
2020-02-24 19:45:19 +0000

    Change pseudo file

--- bash/pseudo/create_pseudo_base_pretrained.sh
+++ bash/pseudo/create_pseudo_base_pretrained.sh
@@ -3,5 +3,5 @@
 python step2_pseudo_labeling/bert-base-pretrained/infer_pseudo.py               \
   --experiment=experiments/1-8-5-head_tail-pretrained-1e-05-210-260-500-26-100   \
   --checkpoint=best_model.pth                                                     \
-  --dataframe=input/sampled_sx_so.csv.gz                                           \
+  --dataframe=input/qa_stackexchange_cleaned.tsv                                   \
   --output_dir=pseudo-predictions/base-pretrained/
